2c2a1d0b7dda4976d80d8140e1d9a42eddf81d59
jumi sanchez <jumary.sanchez@codeimmersives.com>
2020-04-13 11:08:39 -0400

    classy cars

--- main.js
+++ main.js
@@ -1,15 +1,53 @@
 class Vehicle {
+  constructor( name, wheelCount, maxSpeed){
+
+    this.name = name
+    this.wheelCount = wheelCount
+    this.maxSpeed =maxSpeed
+    this.speed = 0
+
+
+  }
+accelerate (){
+  if (this.speed < this.maxSpeed){
+this.speed = this.speed + this.maxSpeed * 1/3
+    }
 }
 
-class Car {
+
 }
 
-class Ferrari {
+class Car extends Vehicle {
+  constructor (name, maxSpeed ){
+
+super(name, 4, maxSpeed)
+  }
+drive (){
+
+  return "Vroom. "
+}
+
 }
 
 
-module.exports = {
-  Vehicle,
-  Car,
-  Ferrari
+class Ferrari  extends Car{
+  constructor(){
+
+    
+    super()
+
+    this.name = "Ferrari"
+    this.maxSpeed = 180
+  }
+  drive(){
+
+    return super.drive() + "I'm a Ferrari!"
+  }
+
+  accelerate (){
+    if (this.speed < this.maxSpeed){
+  this.speed = this.speed + this.maxSpeed * 1/2
+    }
+  }
+  
 }
